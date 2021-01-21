58be65f31c903e93b6aabee943a8d13cf25ede01
Алексей Фоменко <genius690@mail.ru>
2021-01-21 10:51:58 +0700

    f

--- mapa/MainWindow.xaml.cs
+++ mapa/MainWindow.xaml.cs
@@ -23,29 +23,123 @@ using System.Net.Http;
 using System.Web;
 using Newtonsoft.Json;
 
-using mapa.Classes.CodeBeautify;
 
 
-namespace mapa
+
+namespace mapa // сделати список мап обжектов в виде массива, что бы отключать маркеры выбранного исполнителя
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<PointLatLng> areaspots = new List<PointLatLng>();
         List<MapObject> mapObjects = new List<MapObject>();
-        List<MapObject> SortedList = new List<MapObject>();
 
-        List<bandistown> band = new List<bandistown>();
+        public bool bool_first = true;
+
+        public List<GeoClass.Artists[]> artists_list = new List<GeoClass.Artists[]>();
+
+        public List<string> art_pars_list = new List<string>()
+        {
+          "Gordon Rocker" , "Maria Moon" ,  "RYDYR" , "Maroon 5" , "Gorillaz" , "Lil pump" , "Wildways", "Harry" , "Bend" , "Gary lucas" , "Max Leone"
+        };
+
+
+        async Task getResponseAsync(string artName)
+        {
+            //---------------------
+            HttpClient httpClient = new HttpClient();
+            string request = "https://rest.bandsintown.com/v4/artists/" + artName + "/events/?app_id=33ef8f94b2739a88ee6db22fa3ced553";
+            HttpResponseMessage response =
+                (await httpClient.GetAsync(request)).EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            //---------------------
+
+
+            GeoClass.Artists[] artistsFromResponce = GeoClass.Artists.FromJson(responseBody);
+            List<GeoClass.Artists> artistsWithoutSTREAM = new List<GeoClass.Artists>();
+
+            foreach (GeoClass.Artists p in artistsFromResponce)
+            {
+                if (p.Venue.Location != "")
+                {
+                    //------------------------------
+                    artistsWithoutSTREAM.Add(p);
+
+                   
+                    //------------------------------
+                }
+
+               
+            }
+
+            GeoClass.Artists[] artists = artistsWithoutSTREAM.ToArray<GeoClass.Artists>();
+
+            if (artists.Count() != 0)
+            {
+                if ((bool_first == true) && (responseBody != "\n\n[]\n"))
+                {
+                    artists_list.Add(artists);
+                    listbox_artists.Items.Add(artists[0].Lineup[0]);
+                    bool_first = false;
+                    if (artists_list.Count != 0)
+                    {
+                        foreach (GeoClass.Artists i in artists_list.Last())
+                        {
+                            string lat_str = i.Venue.Latitude.Replace(".", ",");
+                            string lng_str = i.Venue.Longitude.Replace(".", ",");
+
+                            double lat = Convert.ToDouble(lat_str);
+                            double lng = Convert.ToDouble(lng_str);
+
+                            MapObject mapObject = new GeoClass(new PointLatLng(lat, lng), i.Venue.Location);
+                            mapObjects.Add(mapObject);
+                            Map.Markers.Add(mapObject.GetMarker());
+                        }
+
+                    }
+                }
+
+                else
+
+                            if ((responseBody != "\n\n[]\n") && (artists[0].ArtistId != artists_list.Last()[0].ArtistId) && (bool_first == false)) // брать первый элемент
+                {
+                    artists_list.Add(artists);
+                    listbox_artists.Items.Add(artists[0].Lineup[0]);
+                    if (artists_list.Count != 0)
+                    {
+                        foreach (GeoClass.Artists i in artists_list.Last())
+                        {
+
+                            string lat_str = i.Venue.Latitude.Replace(".", ",");
+                            string lng_str = i.Venue.Longitude.Replace(".", ",");
+
+                            double lat = Convert.ToDouble(lat_str);
+                            double lng = Convert.ToDouble(lng_str);
+
+                            MapObject mapObject = new GeoClass(new PointLatLng(lat, lng), i.Venue.Location);
+                            mapObjects.Add(mapObject);
+                            Map.Markers.Add(mapObject.GetMarker());
+
+                        }
+
+                    }
+                }
+                
+            }
+            else
+            {
+                lab_art_id.Content = ("Концерты " + artName + " не найдены");
+            };
+        }
 
 
         public MainWindow()
         {
             InitializeComponent();
             initMap();
-            //radioButCreate.IsChecked = true;
-            //createmodecombo.SelectedIndex = 0;
+            pb.Maximum = art_pars_list.Count;
+
         }
 
         public void initMap()
@@ -65,138 +159,18 @@ namespace mapa
         
       
 
-        //private async Task Button_ClickAsync(object sender, RoutedEventArgs e)
-        //{
-        //    HttpClient httpClient = new HttpClient();
-        //    string request = "https://rest.bandsintown.com/v4/artists/maroon%205/events/?app_id=33ef8f94b2739a88ee6db22fa3ced553";
-        //    HttpResponseMessage response =
-        //        (await httpClient.GetAsync(request)).EnsureSuccessStatusCode();
-        //    string responseBody = await response.Content.ReadAsStringAsync();
-        //    band.Add (new bandistown(responseBody));
-        //}
-    
-
-
-    //public void createMarker(List<PointLatLng> points, int index) 
-    //{
-    //    MapObject mapObject = null;
-    //    switch (index)
-    //    {
-    //        case 0:
-    //            {
-    //                mapObject = new Area_class(createObjectName.Text, points);
-    //                break;
-    //            }
-    //        case 1:
-    //            {
-    //                mapObject = new Location_class(createObjectName.Text, points.Last());
-    //                break;
-    //            }
-    //        case 2:
-    //            {
-    //                mapObject = new Car_class(createObjectName.Text, points.Last());
-    //                break;
-    //            }
-    //        case 3:
-    //            {
-    //                mapObject = new Human_class(createObjectName.Text, points.Last());
-    //                break;
-    //            }
-    //        case 4:
-    //            {
-    //                mapObject = new Route_class(createObjectName.Text, points);
-    //                break;
-    //            }
-
-    //    }
-    //    if (mapObject != null)
-    //    {
-    //        mapObjects.Add(mapObject);
-    //        Map.Markers.Add(mapObject.GetMarker());
-    //    }
-    //}
-
-
 
     private void MapLoaded(object sender, RoutedEventArgs e)
         {
 
         }
 
-      
-
-
-
-       
-
-        public List<Artists[]> artists_list = new List<Artists[]>();
-
-        async Task<string> sssAsync ()
-        {
-            HttpClient httpClient = new HttpClient();
-            string request = "https://rest.bandsintown.com/v4/artists/"+ tb_name_art.Text +"/events/?app_id=33ef8f94b2739a88ee6db22fa3ced553";
-            HttpResponseMessage response =
-                (await httpClient.GetAsync(request)).EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-
-            var temp = Artists.FromJson(responseBody);  // - эта сука работает остальное нет 
-
-            if (responseBody != "\n\n[]\n")
-            {
-
-                artists_list.Add(temp);
-
-                //tb2.Text = ochko.Last()[0].Venue.City;
-
-
-            }
-
-            else
-            {
-                System.Windows.MessageBox.Show("Концерты исполнителя не найдены");
-            };
-
-                  
-
-            //bandistown test = new bandistown(tb11.Text);
-
-            //test.govno(tb11.Text);
-
-            //var jopa = JsonConvert.DeserializeObject<bandistown>(tb11.Text);
-
-            //band.Add(new bandistown(tb11.Text));
-
-            //tb11.Text = responseBody;
-
-            return responseBody;
-            
-        }
-
         private async void btn1_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                await sssAsync();
+                await getResponseAsync(tb_name_art.Text);
 
-                if (artists_list.Count != 0)
-                {
-                    foreach (Artists i in artists_list.Last())
-                    {
-                        string lat_str = i.Venue.Latitude.Replace(".", ",");
-
-                        string lng_str = i.Venue.Longitude.Replace(".", ",");
-
-                        double lat = Convert.ToDouble(lat_str);
-                        double lng = Convert.ToDouble(lng_str);
-
-                        MapObject mapObject = new Class1(new PointLatLng(lat, lng), i.Venue.Location);
-                        mapObjects.Add(mapObject);
-                        Map.Markers.Add(mapObject.GetMarker());
-                    }
-
-                    listbox_artists.Items.Add(artists_list.Last()[0].Artist.Name);
-
-                }
             }
             catch (System.Net.Http.HttpRequestException)
             {
@@ -206,11 +180,6 @@ namespace mapa
            
         }
 
-      
-
-     
-        
-
         private void tb_name_art_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             tb_name_art.Text = "";
@@ -219,227 +188,60 @@ namespace mapa
         private void listbox_artists_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             party_list.Items.Clear();
-            foreach (Artists ar in artists_list[listbox_artists.SelectedIndex])
+            foreach (GeoClass.Artists ar in artists_list[listbox_artists.SelectedIndex])
             party_list.Items.Add(ar.Venue.Country + " " + ar.Venue.City + " " + ar.Venue.Name);
+
+            debug.Text = listbox_artists.SelectedIndex.ToString();
         }
 
         private void party_list_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           
+
             if (party_list.SelectedIndex != -1)
-                System.Windows.MessageBox.Show(((artists_list[listbox_artists.SelectedIndex][party_list.SelectedIndex]).Datetime).ToString()); // Map.Position 
+            {
+                debug.Text += party_list.SelectedIndex.ToString();
+
+                debug.Text += (party_list.SelectedIndex + listbox_artists.SelectedIndex + listbox_artists.SelectedIndex + 1).ToString();
+
+                int index = 0;
+
+                for (int i = 0; i < listbox_artists.SelectedIndex; i ++)
+                {
+                    index = index + artists_list[i].Count();
+                }
+                index = index + party_list.SelectedIndex;
+                Map.Position = mapObjects[index].getFocus();   
+                
+                lab_art_date.Content = "Дата проведения: " + (((artists_list[listbox_artists.SelectedIndex][party_list.SelectedIndex]).Datetime).ToString()); // Map.Position 
+                lab_art_id.Content = "Art id - " + (artists_list[listbox_artists.SelectedIndex][0]).ArtistId.ToString();
+                lab_art_counrty.Content = "Страна мероприятия: " + (artists_list[listbox_artists.SelectedIndex][party_list.SelectedIndex]).Venue.Country.ToString();
+                lab_art_city.Content = "Город мероприятия: " + (artists_list[listbox_artists.SelectedIndex][party_list.SelectedIndex]).Venue.City.ToString();
+                lab_art_name.Content = "Название артиста: " + (artists_list[listbox_artists.SelectedIndex][0]).Lineup[0].ToString();
+                lab_art_place.Content = "Название площадки: " + (artists_list[listbox_artists.SelectedIndex][party_list.SelectedIndex]).Venue.Name.ToString();
+            }
         }
 
 
 
 
+        private async void parse_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                foreach (string name in art_pars_list)
+                await getResponseAsync(name);
+            }
+            catch (System.Net.Http.HttpRequestException)
+            {
+                System.Windows.MessageBox.Show("Исполнитель не найден");
+            }
+
+        }
+
+
+        
 
 
 
-
-
-
-
-
-
-
-        //private void Map_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    findsresult.Items.Clear();
-        //    PointLatLng clickedPoint = Map.FromLocalToLatLng((int)e.GetPosition(Map).X, (int)e.GetPosition(Map).Y);
-        //    SortedList = mapObjects.OrderBy(o => o.getDist(clickedPoint)).ToList();
-        //    foreach (MapObject obj in SortedList)
-        //    {
-        //        string mapObjectAndDistanceString = new StringBuilder()
-        //            .Append(obj.getTitle())
-        //            .Append(" - ")
-        //            .Append(obj.getDist(clickedPoint).ToString("0.##"))
-        //            .Append(" м.").ToString();
-        //        findsresult.Items.Add(mapObjectAndDistanceString);
-        //    }
-
-
-        //}
-
-        //private void radioButCreate_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    createmodecombo.IsEnabled = true;
-        //    addbuttoncreate.IsEnabled = true;
-        //    resetpointcreate.IsEnabled = true;
-        //}
-
-        //private void radioButSearch_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    createmodecombo.IsEnabled = false;
-        //    addbuttoncreate.IsEnabled = false;
-        //    resetpointcreate.IsEnabled = false;
-        //}
-
-        //private void addbuttoncreate_Click(object sender, RoutedEventArgs e)
-        //{
-
-        //    if (createObjectName.Text == "")
-        //        System.Windows.MessageBox.Show("Имя объекта пустое");
-        //    else
-        //    {
-        //        createMarker(areaspots, createmodecombo.SelectedIndex);
-        //        areaspots = new List<PointLatLng>();
-        //        createObjectName.Clear();
-        //    }
-        //    checker();
-
-        //}
-
-        //private void Map_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        //{
-        //    double minimum = new double();
-        //    PointLatLng clickedPoint = Map.FromLocalToLatLng((int)e.GetPosition(Map).X, (int)e.GetPosition(Map).Y);
-        //    MapObject @object = null;
-        //    if (mapObjects.Count != 0)
-        //    minimum = mapObjects[0].getDist(clickedPoint);
-        //    foreach (MapObject obj in mapObjects)
-        //    {
-        //        if (minimum > obj.getDist(clickedPoint))
-        //        {
-        //            minimum = obj.getDist(clickedPoint);
-        //            @object = obj;
-        //        }
-        //        if (@object == null)
-        //            @object = mapObjects[0];
-        //    }
-
-        //    if (@object!=null)
-        //    distanceToPoints.Content = $"{Math.Round(minimum)} { @object.getTitle()}"; 
-
-        //}
-
-        //void checker()
-        //{
-        //    switch (createmodecombo.SelectedIndex)
-        //    {
-        //        case 0:
-        //            {
-        //                if (areaspots.Count > 2)
-        //                    addbuttoncreate.IsEnabled = true;
-        //                else
-        //                    addbuttoncreate.IsEnabled = false;
-        //                break;
-        //            }
-        //        case 4:
-        //            {
-        //                if (areaspots.Count > 1)
-        //                    addbuttoncreate.IsEnabled = true;
-        //                else
-        //                    addbuttoncreate.IsEnabled = false;
-        //                break;
-        //            }
-        //        default :
-        //            {
-        //                addbuttoncreate.IsEnabled = true;
-        //                break;
-        //            }
-
-        //    }
-
-        //    if (areaspots.Count == 0)
-        //        addbuttoncreate.IsEnabled = false;
-        //}
-
-        //private void Map_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    areaspots.Add(Map.FromLocalToLatLng((int)e.GetPosition(Map).X, (int)e.GetPosition(Map).Y));
-        //    checker();
-        //    clickinfoY.Content = areaspots.Last().Lng;
-        //    clickinfoX.Content = areaspots.Last().Lat;
-        //}
-
-        //private void createmodecombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    checker();
-        //}
-
-        //private void findsresult_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-
-
-        //        if (SortedList.Count != 0 && findsresult.SelectedIndex != -1)
-        //            Map.Position = SortedList[findsresult.SelectedIndex].getFocus();
-
-        //}
-
-
-
-
-        //private void SearchButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    findsresult.Items.Clear();
-        //    for (int i = 0; i < mapObjects.Count; i++)
-        //        if (mapObjects[i].objectName.Contains(whatineedtofound.Text))
-        //        {
-        //            findsresult.Items.Add(mapObjects[i].objectName);
-        //            SortedList.Add(mapObjects[i]);
-        //        }
-        //}
-
-        //private void resetpointcreate_Click(object sender, RoutedEventArgs e)
-        //{
-        //    areaspots = new List<PointLatLng>();
-        //    clickinfoX.Content = "0";
-        //    clickinfoY.Content = "0";
-
-        //}
-    }
-    
-}
-
-
-
-
-//public ref GMapMarker findRef()
-//{
-//    return ref new GMapMarker(new PointLatLng());
-//}
-
-//PointLatLng point = new PointLatLng(55.016511, 82.946152);
-
-//GMapMarker marker = new GMapMarker(point)
-//{
-//    Shape = new Image
-//    {
-//        Width = 32, // ширина маркера
-//        Height = 32, // высота маркера
-//        ToolTip = "Honda CR-V", // всплывающая подсказка
-//        Source = new BitmapImage(new Uri("pack://application:,,,/Resources/men.png")) // картинка
-//    }
-//};
-//Map.Markers.Add(marker);
-
-//GMapMarker marker = new GMapMarker(point)
-//{
-//    Shape = new Image
-//    {
-//        Width = 32, // ширина маркера
-//        Height = 32, // высота маркера
-//        ToolTip = "timetime", // всплывающая подсказка
-//        Source = new BitmapImage(new Uri("pack://application:,,,/Resources/notMainSpot.png")) // картинка
-//    }
-//};
-
-
-
-
-
-//if (Map.Markers.Count == 0)
-//{
-//    Map.Markers.Add(marker);
-//    clickinfoX.Content = point.Lat;
-//    clickinfoY.Content = point.Lng;
-
-//}
-//else
-//{
-
-//    Map.Markers.Add(marker);     // после выхода из метода ссылка обнуляется 
-//    clickinfoX.Content = point.Lat;
-//    clickinfoY.Content = point.Lng;
-//}
+        
